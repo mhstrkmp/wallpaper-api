1f3853eb1851ed787fa3f829b9d40c2a63d96048
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-27 22:41:54 +0100

    :poop: Add some onclick functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import ImagePreview from "./components/ImagePreview";
 import getRandomImage from "./utils/api";
 
 function App() {
+  const [randomImage, setRandomImage] = useState(null);
+
   return (
     <main>
-      <button onClick={() => getRandomImage()}>Get Random Image</button>
+      <button
+        onClick={() => {
+          setRandomImage(getRandomImage());
+          console.log(randomImage);
+        }}
+      >
+        Get Random Image
+      </button>
       <ImagePreview
         src={
           "https://images.unsplash.com/photo-1603614486387-276f74fcbe2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80"
