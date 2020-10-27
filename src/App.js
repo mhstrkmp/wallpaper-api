53b19649463372cc222d21c9971dc9598a35ea24
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-27 17:39:25 +0100

    :sparkles: Add get random image api function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import ImagePreview from "./components/ImagePreview";
+import getRandomImage from "./utils/api";
 
 function App() {
   return (
     <main>
-      <button>Get Random Image</button>
+      <button onClick={() => getRandomImage()}>Get Random Image</button>
       <ImagePreview
         src={
           "https://images.unsplash.com/photo-1603614486387-276f74fcbe2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80"
