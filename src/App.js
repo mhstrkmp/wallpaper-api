436619e927a10af755cc36fd40f62d0959e17db3
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-27 15:50:45 +0100

    :sparkles: Add Image preview component"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import ImagePreview from "./components/ImagePreview";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <button>Get Random Image</button>
+      <ImagePreview
+        src={
+          "https://images.unsplash.com/photo-1603614486387-276f74fcbe2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80"
+        }
+        alt={"Newspaper reading"}
+        author={"Matthias"}
+      />
+    </main>
+  );
 }
 
 export default App;
