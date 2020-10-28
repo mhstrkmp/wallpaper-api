90b60d30aeebba0ce92803f9b836fa4b97ae2a63
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-28 16:28:49 +0100

    :sparkles: Add favorite id to local storage

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -2,10 +2,29 @@ import React from "react";
 import IconButton from "./IconButton";
 import "./imagePreview.css";
 
-export default function ImagePreview({ src, alt, author }) {
+export default function ImagePreview({ src, alt, author, id }) {
   return (
     <div className="imageContainer">
-      <IconButton onClick={() => alert("Clicked")}>❤️</IconButton>
+      <IconButton
+        onClick={() => {
+          let favorites = null;
+          try {
+            favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+          } catch (error) {
+            // Parsing might fail
+            console.log(error);
+            favorites = [];
+          }
+          if (favorites.includes(id)) {
+            // Already added to favorites
+            return;
+          }
+          const newFavorites = [...favorites, id];
+          localStorage.setItem("favorites", JSON.stringify(newFavorites));
+        }}
+      >
+        ❤️
+      </IconButton>
       <img className="imageThumb" src={src} alt={alt} />
       <p className="imageAuthor">Author: {author}</p>
     </div>
